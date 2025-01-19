8b6142804060ef9c7b418581d8a8f6c50629f79f
Lukas O. <93264203+LukasOlivier@users.noreply.github.com>
2025-01-19 19:13:41 +0100

    archiving this site

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,26 @@
-import './globals.css'
+import './globals.css';
 
 export const metadata = {
   title: "Lukas Olivier's Blog",
   description: 'Read everything about Lukas Olivier, student at Howest Brugge',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en"  style={{ overscrollBehavior: 'none' }} className='text-white bg-gradient-to-r from-slate-950 to-zinc-900'>
-      <body >{children}</body>
+    <html
+      lang="en"
+      style={{ overscrollBehavior: 'none' }}
+      className="text-white bg-gradient-to-r from-slate-950 to-zinc-900"
+    >
+      <head>
+        {/* Prevent indexing */}
+        <meta name="robots" content="noindex, nofollow" />
+      </head>
+      <body>{children}</body>
     </html>
-  )
+  );
 }
